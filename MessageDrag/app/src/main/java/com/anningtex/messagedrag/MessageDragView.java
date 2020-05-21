31ef99e90d6bfd8844dbbd45ab2a7d5079e53c69
hanjunshuaii <1433452759@qq.com>
2020-05-22 00:12:53 +0800

    粘性效果

--- MessageDrag/app/src/main/java/com/anningtex/messagedrag/MessageDragView.java
+++ MessageDrag/app/src/main/java/com/anningtex/messagedrag/MessageDragView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -67,12 +68,72 @@ public class MessageDragView extends View {
         canvas.drawCircle(mDragPointF.x, mDragPointF.y, mDragRadius, mPaint);
 
         // 固定圆
-        float distance = (float) getDistance(mFixedPointF, mDragPointF);
-        mFixedRadius = dip2px(mFixedRadiusMax - distance / 14);
-        if (mFixedRadius > mFixedRadiusMin) {
+//        float distance = (float) getDistance(mFixedPointF, mDragPointF);
+//        mFixedRadius = (int) (mFixedRadiusMax - distance / 14);
+        // 路径
+        Path bazeierPath = getBaZeiErPath();
+        if (bazeierPath != null) {
+            // 小到一定程度就不见了（不画了）
             canvas.drawCircle(mFixedPointF.x, mFixedPointF.y, mFixedRadius, mPaint);
+            // 画贝塞尔曲线
+            canvas.drawPath(bazeierPath, mPaint);
         }
+    }
 
+    /**
+     * 获取贝塞尔的路径
+     *
+     * @return
+     */
+    private Path getBaZeiErPath() {
+        float distance = (float) getDistance(mFixedPointF, mDragPointF);
+        mFixedRadius = (int) (mFixedRadiusMax - distance / 14);
+
+        if (mFixedRadius < mFixedRadiusMin) {
+            //超过一定距离 贝塞尔和固定圆就不画了
+            return null;
+        }
+        Path bezeierPath = new Path();
+        // 求斜率
+        float dy = (mDragPointF.y - mFixedPointF.y);
+        float dx = (mDragPointF.x - mFixedPointF.x);
+        float tanA = dy / dx;
+        // 求角 a
+        double aTanA = Math.atan(tanA);
+
+        // p0
+        float p0x = (float) (mFixedPointF.x + mFixedRadius * Math.sin(aTanA));
+        float p0y = (float) (mFixedPointF.y - mFixedRadius * Math.cos(aTanA));
+
+        // p1
+        float p1x = (float) (mDragPointF.x + mFixedRadius * Math.sin(aTanA));
+        float p1y = (float) (mDragPointF.y - mFixedRadius * Math.cos(aTanA));
+
+        // p2
+        float p2x = (float) (mDragPointF.x - mFixedRadius * Math.sin(aTanA));
+        float p2y = (float) (mDragPointF.y + mFixedRadius * Math.cos(aTanA)); // p1
+
+        // p3
+        float p3x = (float) (mFixedPointF.x - mFixedRadius * Math.sin(aTanA));
+        float p3y = (float) (mFixedPointF.y + mFixedRadius * Math.cos(aTanA));
+
+        // 拼装 贝塞尔曲线路径
+        bezeierPath.moveTo(p0x, p0y);
+        // 两个点 第一个点（控制点,两个圆心的中心点）
+        PointF controlPoint = getControlPoint();
+        // 画了第一条
+        bezeierPath.quadTo(controlPoint.x, controlPoint.y, p1x, p1y);
+
+        // 画第二条
+        bezeierPath.lineTo(p2x, p2y);
+        bezeierPath.quadTo(controlPoint.x, controlPoint.y, p3x, p3y);
+        bezeierPath.close();
+        return bezeierPath;
+    }
+
+    private PointF getControlPoint() {
+        return new PointF(mFixedPointF.x + (mDragPointF.x - mFixedPointF.x) / 2,
+                mFixedPointF.y + (mDragPointF.y - mFixedPointF.y) / 2);
     }
 
     private double getDistance(PointF point1, PointF point2) {
